91ba3cc073fbd49e873f9e1cfac6d2b9822deafd
Sathishnaik786 <sathishnaikislavath@gmail.com>
2026-03-02 23:53:55 +0530

    style: increase navbar logo size further for better visibility

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -40,7 +40,7 @@ const Navbar = () => {
                         <img
                             src="/logo.png"
                             alt="Great Work - 100% Genuine Work"
-                            className="h-[58px] md:h-[68px] lg:h-[84px] w-auto object-contain group-hover:scale-105 transition-transform duration-300 drop-shadow-md"
+                            className="h-[68px] md:h-[78px] lg:h-[96px] w-auto object-contain group-hover:scale-105 transition-transform duration-300 drop-shadow-md py-1"
                         />
                     </Link>
 
@@ -103,7 +103,7 @@ const Navbar = () => {
                             <img
                                 src="/logo.png"
                                 alt="Great Work"
-                                className="h-[68px] w-auto object-contain"
+                                className="h-[78px] w-auto object-contain"
                             />
                             <button onClick={() => setMobileMenuOpen(false)} className="text-white"><X size={28} /></button>
                         </div>
